9c63da40e98ced31c579bc8e22858682dcb5d3e0
Utkarsh Singh <utkarsdevendrasingh@gmail.com>
2025-08-03 00:50:19 +0530

    prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,22 @@
 import express from 'express'
 const app = express();
 import cors from 'cors'
+import path from 'path'
 import { connectDB } from './config/db.js';
 import notesRoutes from './routes/notesRoutes.js'
 import rateLimiter from '../middleware/rateLimiter.js';
+const __dirname = path.resolve();
 app.use(cors())
 app.use(rateLimiter)
 app.use(express.json())
 app.use('/api/route',notesRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+app.use(express.static(path.join(__dirname,'../frontend/dist')))
+app.use('*',(req,res)=>{
+   res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})
+}
 connectDB().then(()=>{
       app.listen(process.env.PORT || 3000,()=>{
       console.log("sever runnig on 3000")
