44a03c6d7b835628de8306fedb9d0539682b4f5e
Utkarsh Singh <utkarsdevendrasingh@gmail.com>
2025-08-24 17:57:44 +0530

    app.jsx fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import Homepage from './pages/Homepage'
+import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 // import toast from 'react-hot-toast'
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Homepage/>} />
+        <Route path='/' element={<HomePage/>} />
         <Route path='/create' element={<CreatePage/>}/>
       </Routes>
     </>
