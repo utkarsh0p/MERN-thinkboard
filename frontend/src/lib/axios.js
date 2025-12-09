624efc48b44982270585e59b8d625927e8723da6
Utkarsh Singh <utkarshdevendrasingh@gamil.com>
2025-12-09 07:52:46 +0530

    add public ipv4

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL =
-  import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api';
+  import.meta.env.MODE === 'development' ? 'http://15.206.149.219:5000' : '/api';
 export const api = axios.create({
   baseURL: BASE_URL,
 });
